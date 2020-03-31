52ef769a235926ec109e28000cafc3b6c05fa3c5
Sina Dashti <sina.v.dashti@gmail.com>
2020-03-31 03:25:42 +0200

    solution of py-check-subset-English

--- hacker_python.py
+++ hacker_python.py
@@ -748,15 +748,26 @@
 ################################################################################
                                      #A
 ################################################################################
-_, args = input(), input().split()
-print(*[i for i in set(sorted(args)) if args.count(i) == 1])
+# _, args = input(), input().split()
+# print(*[i for i in set(sorted(args)) if args.count(i) == 1])
 ################################################################################
-members = input().split()
-rooms = set()   # Contains all the rooms.
-room_more_mem = set()   # Contains only the rooms with families.
-
-for m in members:
-    if m not in room_more_mem:
-        target = room_more_mem if m in rooms else rooms
-        target.add(m)
-print(rooms.difference(room_more_mem).pop())
+# members = input().split()
+# rooms = set()   # Contains all the rooms.
+# room_more_mem = set()   # Contains only the rooms with families.
+#
+# for m in members:
+#     if m not in room_more_mem:
+#         target = room_more_mem if m in rooms else rooms
+#         target.add(m)
+# print(rooms.difference(room_more_mem).pop())
+################################################################################
+                                      #Q
+################################################################################
+# py-check-subset-English
+################################################################################
+################################################################################
+                                     #A
+################################################################################
+for _ in range(int(input())):
+    _, A, _, B = input(), set(input().split()), input(), set(input().split())
+    print(A.issubset(B))
