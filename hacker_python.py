7617a12379b34699674cec181ab983e943d3ce65
Sina Dashti <sina.v.dashti@gmail.com>
2020-04-26 00:17:42 +0200

    solution of validating-uid

--- hacker_python.py
+++ hacker_python.py
@@ -1179,13 +1179,43 @@
 ################################################################################
                                      #A
 ################################################################################
-from html.parser import HTMLParser
-class MyHTMLParser(HTMLParser):
-    def handle_starttag(self, tag, attrs):
-        print(tag)
-        [print('-> {} > {}'.format(*attr)) for attr in attrs]
+# from html.parser import HTMLParser
+# class MyHTMLParser(HTMLParser):
+#     def handle_starttag(self, tag, attrs):
+#         print(tag)
+#         [print('-> {} > {}'.format(*attr)) for attr in attrs]
+#
+# html = '\n'.join([input() for _ in range(int(input()))])
+# parser = MyHTMLParser()
+# parser.feed(html)
+# parser.close()
+################################################################################
+################################################################################
+                                      #Q
+################################################################################
+# validating-uid-English
+################################################################################
+################################################################################
+                                     #A
+################################################################################
+import re
+for _ in range(int(input())):
+    s = input()
+    print('Valid' if all([re.search(r, s)
+    for r in [r'[A-Za-z0-9]{10}', r'([A-Z].*){2}', r'([0-9].*){3}']])
+    and not re.search(r'(.).*\1', s)
+    else 'Invalid')
 
-html = '\n'.join([input() for _ in range(int(input()))])
-parser = MyHTMLParser()
-parser.feed(html)
-parser.close()
+# def is_valid(uid):
+#     has_2_or_more_upper = bool(re.search(r'[A-Z]{2,}', uid))
+#     has_3_or_more_digits = bool(re.search(r'\d{3,}', uid))
+#     has_10_proper_elements = bool(re.match(r'^[a-zA-Z0-9]{10}$', uid))
+#     no_repeats = not bool(re.search(r'(.)\1', uid))
+#
+#     if has_2_or_more_upper and has_3_or_more_digits and has_10_proper_elements and no_repeats:
+#         return "Valid"
+#     else:
+#         return "Invalid"
+#
+# for _ in range(int(input())):
+#     print is_valid(input())
